7cfd478b95c6795ddb079e9adf3a9188f6b0c0d0
Toshe Boshkovski <tboshkovski@airfind.com>
2022-12-08 08:00:26 +0100

    4.2 solved

---
+++ 4/2/index.ts
@@ -0,0 +1,33 @@
+import { processFile } from "../../process-file";
+
+const range = (data: string) => {
+  const [first, second] = data.split("-");
+  const f = Number(first);
+  const s = Number(second);
+  const result = new Array(s - f).fill(1).map((_, k) => k + f);
+  result.push(s);
+  return result;
+};
+
+const result = async () => {
+  const stream = await processFile("4/data.txt");
+
+  let contained = 0;
+
+  for await (const line of stream) {
+    const [first, second] = line.split(",");
+    const pair = {
+      first: range(first),
+      second: range(second),
+    };
+    if (
+      pair.first.some((entry) => pair.second.includes(entry)) ||
+      pair.second.some((entry) => pair.first.includes(entry))
+    ) {
+      contained++;
+    }
+  }
+  return contained;
+};
+
+result().then((data) => console.log(data));
