6f4b014ca21173b55f543b87dd0d84fa3485d2a4
Toshe Boshkovski <tboshkovski@airfind.com>
2022-12-13 08:13:55 +0100

    12.1

--- 12/1/Hill.ts
+++ 12/1/Hill.ts
@@ -17,33 +17,37 @@ export class Path {
   constructor(
     data: ID,
     value: string | number,
+    char: string,
     steps: number,
     previous: Path,
     next: Path
   ) {
     this.data = data;
+    this.steps = steps;
+    this.char = char;
     this.value = value;
     this.previous = previous;
     this.next = next;
     this.steps = steps;
-    if (typeof value === 'string') this.char = value;
-    else this.char = String.fromCharCode(97 + value);
   }
 }
 
+const chars = ['S', 'U'];
+
 export class Hill {
   // steps = weight of Path, starting weight is 0 for root Path
-  paths: Path[] = [];
   root: Path;
   stack: Path[] = [];
+  paths = [];
+  map: any[] = [];
 
-  private visited(node: Path, next: { row: number; column: number }) {
-    if (node === null || node?.previous === null) return;
+  private visited(node: Path, current: { row: number; column: number }) {
+    if (node === null || node?.previous === null) return false;
     let iter = node;
-    const { row, column } = next;
+    const { row, column } = current;
     while (iter !== null) {
       if (iter.data.column === column && iter.data.row === row) return true;
-      if (iter.previous === null) break;
+      if (iter.previous === null || iter.previous.char === 'S') break;
       iter = iter.previous;
     }
     return false;
@@ -51,8 +55,8 @@ export class Hill {
 
   private climbable(node: Path, data: string | number) {
     if (node === null) return true;
-    const { value } = node;
-    if (value === 'E') return false;
+    const { char, value } = node;
+    if (char === 'E') return false;
     if (data === 'E') {
       return true;
     }
@@ -63,26 +67,38 @@ export class Hill {
     }
     return false;
   }
+  private inBounds(row: number, column: number, node: Path) {
+    if (row < 0 || row > this.map.length - 1) return false;
+    if (column < 0 || column > this.map[row].length - 1) return false;
+    return true;
+  }
 
-  addPath(
-    data: ID,
-    element: string | number,
-    previous: Path = null,
-    next: Path = null
-  ) {
+  addPath(data: ID, previous: Path = null, next: Path = null) {
     const { row, column } = data;
     const obj = {
       row,
       column,
     };
-    const canBeClimbed = this.climbable(previous, element);
-    if (!canBeClimbed) return;
+    const specials = ['S', 'E'];
+    const inBounds = this.inBounds(row, column, previous);
+    if (inBounds === false) return;
+    const element = this.map[row][column];
+    if (element === 'E')
+      this.paths.push(
+        new Path(obj, 'E', 'E', previous.steps + 1, previous, next)
+      );
+    const current =
+      typeof element === 'string' && !specials.includes(element)
+        ? element.charCodeAt(0) - 96
+        : element;
+    const climbable = this.climbable(previous, current);
+    if (climbable === false) return;
     if (previous !== null) {
       const wasVisited = this.visited(previous, data);
       if (wasVisited) return;
     }
     const steps = previous === null ? 0 : previous?.steps + 1;
-    const node = new Path(obj, element, steps, previous, next);
+    const node = new Path(obj, current, element, steps, previous, next);
     const debug = 'as';
     return node;
   }
--- 12/1/index .ts
+++ 12/1/index .ts
@@ -4,104 +4,34 @@ import { Hill, Path } from './Hill';
 const result = async () => {
   const stream = await processFile('12/dummy.txt');
   const hill = new Hill();
-  const map = [];
   let row = 0;
 
   for await (const line of stream) {
     const len = line.length;
-    map[row] = new Array(len);
-    map[row] = line.split('').map((data) => {
-      const lowercase = data === data.toLowerCase();
-      if (!lowercase) {
-        if (data === 'S') return 'S';
-        else if (data === 'E') return 'E';
-      }
-      const result = data.charCodeAt(0) - 96;
-      return result;
-    });
+    hill.map[row] = new Array(len);
+    hill.map[row] = line.split('').map((data) => data);
     row++;
   }
   const queue: Path[] = [];
   // initiate siblings
   const root = { row: 0, column: 0 };
-  hill.root = hill.addPath(root, map[0][0]);
-  const sRight = hill.addPath({ row: 0, column: 1 }, map[0][1], hill.root);
-  const sDown = hill.addPath({ row: 1, column: 0 }, map[1][0], hill.root);
-  queue.push(sRight, sDown);
+  hill.root = hill.addPath(root);
+  const right = hill.addPath({ row: 0, column: 1 }, hill.root);
+  const down = hill.addPath({ row: 1, column: 0 }, hill.root);
+  if (right) queue.push(right);
+  if (down) queue.push(down);
 
   while (queue.length > 0) {
     const node = queue.pop();
-    const rows = map.length - 1;
-    const columns = map[node.data.row].length - 1;
     const { row, column } = node.data;
-    const innerQueue: Path[] = [];
-    const l = map[row][column - 1];
-    const r = map[row][column + 1];
-    const u = map[row + 1][column];
-    const d = map[row - 1][column - 1];
-    const left = hill.addPath({ row, column: column - 1 }, l, node);
-    const right = hill.addPath({ row, column: column + 1 }, r, node);
-    const up = hill.addPath({ row: row + 1, column }, u, node);
-    const down = hill.addPath({ row: row - 1, column }, d, node);
-
-    // checkRight: if (node.data.column < columns && node.data.row === row) {
-    //   const next = hill.addPath(
-    //     { row, column: column + 1 },
-    //     map[row][column + 1],
-    //     node
-    //   );
-    //   if (next) {
-    //     if (next.value === 'E') {
-    //       hill.paths.push(next);
-    //       continue;
-    //     }
-    //     innerQueue.push(next);
-    //   }
-    // }
-    // checkLeft: if (node.data.column > 0 && node.data.row === row) {
-    //   const next = hill.addPath(
-    //     { row, column: column - 1 },
-    //     map[row][column - 1],
-    //     node
-    //   );
-    //   if (next) {
-    //     if (next.value === 'E') {
-    //       hill.paths.push(next);
-    //       continue;
-    //     }
-    //     innerQueue.push(next);
-    //   }
-    // }
-    // checkTop: if (node.data.row < rows && node.data.column === column) {
-    //   const next = hill.addPath(
-    //     { row: row + 1, column },
-    //     map[row + 1][column],
-    //     node
-    //   );
-    //   if (next) {
-    //     if (next.value === 'E') {
-    //       hill.paths.push(next);
-    //       continue;
-    //     }
-    //     innerQueue.push(next);
-    //   }
-    // }
-
-    // checkBottom: if (node.data.row > 0 && node.data.column === column) {
-    //   const next = hill.addPath(
-    //     { row: row - 1, column },
-    //     map[row - 1][column],
-    //     node
-    //   );
-    //   if (next) {
-    //     if (next.value === 'E') {
-    //       hill.paths.push(next);
-    //       continue;
-    //     }
-    //     innerQueue.push(next);
-    //   }
-    // }
-    queue.push(...innerQueue);
+    const left = hill.addPath({ row, column: column - 1 }, node);
+    const right = hill.addPath({ row, column: column + 1 }, node);
+    const down = hill.addPath({ row: row + 1, column }, node);
+    const up = hill.addPath({ row: row - 1, column }, node);
+    if (left) queue.push(left);
+    if (right) queue.push(right);
+    if (down) queue.push(down);
+    if (up) queue.push(up);
     const debug = '';
   }
   const valid = hill.stack.filter((node) => node?.value === 'E');
